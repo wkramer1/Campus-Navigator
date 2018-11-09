26a81cc3d13a4b283be380d1dad8c79a2d196e8c
Ryan Hiller <ryanhiller@growhousegaming.com>
2018-11-09 09:39:02 -0700

    Fully functional side drawer menu for mobile devices.

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -8,17 +8,33 @@
 
 import React from "react";
 
-import classes from "./SideDrawer.css";
+import Aux from "../../../hoc/Auxiliary";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 import NavItems from "../NavItems/NavItems";
 import RegisterNav from "../RegisterNav/RegisterNav";
 
+import classes from "./SideDrawer.css";
+
 const sideDrawer = props => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
     return (
-        <div className={classes.SideDrawer}>
-            {/*Logo*/}
-            <NavItems />
-            <RegisterNav />
-        </div>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.LogoNav}>
+                    <img
+                        className={classes.DrawerLogo}
+                        src={require("../../../assets/images/logo02.png")}
+                        alt="Campus Navigator Logo"
+                    />
+                    <NavItems />
+                </div>
+                <RegisterNav />
+            </div>
+        </Aux>
     );
 };
 
